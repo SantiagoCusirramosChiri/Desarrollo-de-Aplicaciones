f3b72962866aaf949c1cd78929c9aa6baf8741f4
SantiagoCusirramosChiri <santiago.cusirramos@ucsm.edu.pe>
2025-08-23 15:18:29 -0500

    Se sube el ejercicio 2 - formulario

---
+++ F1/Pract1 - HTML/Ejercicios/ej2/script.js
@@ -0,0 +1,49 @@
+function validarPassword() {
+    const password = document.getElementById("password");
+    const confirmar = document.getElementById("confirmarPassword");
+    const errorDiv = document.getElementById("errorPassword");
+
+    if(password.value !== confirmar.value) {
+        confirmar.setCustomValidity("Todo mal bro");
+        errorDiv.style.display = "block";
+    } else {
+        confirmar.setCustomValidity('');
+        errorDiv.style.display = "none";
+    }
+}
+
+// function verificarGenero(select) {
+//     const mensaje = document.getElementById("mensaje-genero");
+//
+//     if(select.value == 'otro') {
+//         mensaje.style.display = "block";
+//         mensaje.classList.add('hidden');
+//     } else {
+//         mensaje.style.display = "none";
+//         mensaje.classList.remove('hidden');
+//     }
+// }
+
+function verificarGenero(select) {
+    const mensaje = document.getElementById('mensaje-genero');
+    mensaje.style.display = select.value === 'otro' ? 'block' : 'none';
+}
+
+document.addEventListener('DOMContentLoaded', function (){
+    document.getElementById('registroForm').addEventListener('submit', function (event) {
+        if (!this.checkValidity()) {
+            event.preventDefault();
+
+            const inputs = this.inputs.querySelectorAll('input');
+            inputs.forEach(input => {
+                if (!input.validity.valid) {
+                    input.reportValidity();
+                }
+            });
+
+        } else {
+            event.preventDefault();
+            alert("Formulario valido, eso creo, no se que acabo de codear ._.")
+        }
+    });
+})
