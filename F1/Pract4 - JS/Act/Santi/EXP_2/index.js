cdbc8259901cc7e7bdb3d0b652a272d97b97722d
SantiagoCusirramosChiri <santiago.cusirramos@ucsm.edu.pe>
2025-09-10 23:50:03 -0500

    lab 4 - act 2 avance

---
+++ F1/Pract4 - JS/Act/Santi/EXP_2/index.js
@@ -0,0 +1,22 @@
+// ----------- parte A
+
+if(true) {
+    let var1 = 'f';
+    const var2 = 'a';
+
+    console.log(var2);
+    const var3 = 'b';
+    console.log(var3);
+}
+
+// ----------- parte B
+
+console.log(x === undefined);
+x = 3;
+
+var myvar = 'valor random, o indefinido';
+
+(function () {
+    console.log(myvar);
+    var myvar = 'valor local';
+})();
