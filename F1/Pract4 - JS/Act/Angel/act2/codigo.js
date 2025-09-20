560b1d2fde1bd79f0720ee02a59a76db17b0f811
AngelSalasZapata <164601086+AngelSalasZapata@users.noreply.github.com>
2025-09-19 20:47:30 -0500

    Update codigo.js

--- F1/Pract4 - JS/Act/Angel/act2/codigo.js
+++ F1/Pract4 - JS/Act/Angel/act2/codigo.js
@@ -1,3 +1,4 @@
+
 {
     //variable con ambito de bloque
     let superfighter = "soy un superluchador";
@@ -22,3 +23,33 @@ try {
 } catch (error) {
     console.log("Error al acceder a la constante de bloque:", error.message);
 }
+
+console.log(x === undefined); //true
+var x = 3;
+//devolverá un valor de undefined
+var mivariable = 'hola que hace';
+(function() {
+    console.log(mivariable); //undefined
+    var mivariable = 'valor local';}
+)();
+
+var answer = 42;
+answer = 'Gracias por todo Barcelona de mi vida...';
+
+parseInt("F", 16);
+parseInt("17", 8);
+parseInt("15", 10);
+parseInt(15.99, 10);
+parseInt("FXX123", 16);
+parseInt("1111", 2);
+parseInt("15*3", 10);
+parseInt("12", 13);
+parseInt("Holanda", 8); //No es un número en absoluto
+parseInt("0x7", 10); //No es de base 10
+parseInt("546", 2); //Los dígitos no son válidos para representaciones binarias.
+
+var howMany = 10;
+alert("howMany.toString() is " + howMany.toString()); // displays "10"
+alert("45 .toString() is " + 45 .toString()); //displays "45"
+var x = 7;
+alert(x.toString(2)) // Displays "111"
