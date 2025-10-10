a1f8bde6177973f4ac75171fac7876e781e470c5
AngelSalasZapata <angel.salas@ucsm.edu.pe>
2025-10-10 08:35:32 -0500

    codigos de los archivos js

---
+++ F2/pract_07/src/act/Angel/act1/HomePage.js
@@ -0,0 +1,8 @@
+export const HomePage = () => {
+ return (
+ <div>
+ <h2>Bienvenido a nuestra pag de iniciooo</h2>
+ <p>Utiliza la navegación para explorar nuestros cursos xd</p>
+ </div>
+ );
+};
---
+++ F2/pract_07/src/act/Angel/act1/NotFoundPage.js
@@ -0,0 +1,1 @@
+export const NotFoundPage = () => <h2>Error 404 - sorry amigazo la pág no existe :c</h2>;
