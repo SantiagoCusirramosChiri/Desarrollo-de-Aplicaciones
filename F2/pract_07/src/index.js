c4fd14251b6568b5eea43ad8c4242f51e7f60ebc
SantiagoCusirramosChiri <santiago.cusirramos@ucsm.edu.pe>
2025-10-16 09:36:21 -0500

    update

--- F2/pract_07/src/index.js
+++ F2/pract_07/src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './original/index.css';
-import App from './original/App';
 import reportWebVitals from './original/reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Layout from './act/santi/act_1/Layout';
+import { HomePage } from './act/santi/act_1HomePage';
+import { CoursesPage } from './act/santi/act_1CoursesPage';
+import { CourseDetailPage } from './act/santi/act_1CourseDetailPage';
+import { NotFoundPage } from './act/santi/act_1NotFoundPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+
+                <Route path="/" element={<Layout />}>
+
+                    <Route index element={<HomePage />} />
+
+                    <Route path="cursos" element={<CoursesPage />} />
+                    <Route path="cursos/:courseId" element={<CourseDetailPage />} />
+
+                    <Route path="*" element={<NotFoundPage />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
